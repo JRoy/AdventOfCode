5e57f365ee699d7e053c263d34b1b0a8bd657683
JRoy <joshroy126@gmail.com>
2019-12-02 13:09:57 -0500

    Day 2: Properly clone list in part 2

--- src/main/java/io/github/jroy/advent/twentynineteen/daytwo/PartTwo.java
+++ src/main/java/io/github/jroy/advent/twentynineteen/daytwo/PartTwo.java
@@ -3,6 +3,8 @@ package io.github.jroy.advent.twentynineteen.daytwo;
 import io.github.jroy.advent.common.Utils;
 
 import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PartTwo {
@@ -11,11 +13,16 @@ public class PartTwo {
     List<Integer> code = PartOne.createIntCode(Utils.readResourceLines("twentynineteen/daytwo.input").get(0));
     for (int noun = 0; noun < 100; ++noun) {
       for (int verb = 0; verb < 100; ++verb) {
-        code.set(1, noun);
-        code.set(2, verb);
-        if (PartOne.processIntCode(code).get(0).equals(19690720)) {
-          System.out.println("Part Two Answer: " + (100 * noun + verb));
-          System.exit(0);
+        List<Integer> caseCode = new ArrayList<>(code);
+        caseCode.set(1, noun);
+        caseCode.set(2, verb);
+        try {
+          if (PartOne.processIntCode(caseCode).get(0).equals(19690720)) {
+            System.out.println("Part Two Answer: " + (100 * noun + verb));
+            System.exit(0);
+          }
+        } catch (InvalidObjectException ignored) {
+          //This noun and verb pair produces invalid Intcode
         }
       }
     }
