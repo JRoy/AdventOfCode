a5a004bae1597f6fba70446f1be6c1e4411cd99d
JRoy <joshroy126@gmail.com>
2020-12-01 08:37:51 -0500

    2020 - Add Util method to automatically get day's input

--- src/main/java/io/github/jroy/advent/common/Utils.java
+++ src/main/java/io/github/jroy/advent/common/Utils.java
@@ -6,11 +6,28 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utils {
 
+  private final static Pattern PACKAGE_PATTERN = Pattern.compile("io\\.github\\.jroy\\.advent\\.(\\w+)\\.(\\w+)\\..+");
+
   public static List<String> readResourceLines(String name) throws IOException {
     return FileUtils.readLines(new File(Utils.class.getResource("/" + name).getPath()), "utf-8");
+  }
+
+  public static List<String> getDayInput() throws IOException {
+    StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+    for (StackTraceElement element : elements) {
+      if (!element.getClassName().equals(Utils.class.getName()) && !element.getClassName().contains("java.lang.Thread")) {
+        Matcher matcher = PACKAGE_PATTERN.matcher(element.getClassName());
+        if (matcher.matches()) {
+          return readResourceLines(matcher.group(1) + "/" + matcher.group(2) + ".input");
+        }
+      }
+    }
+    throw new IllegalCallerException("Method called out of correct package spec! (Update regex?)");
   }
 
   /**
